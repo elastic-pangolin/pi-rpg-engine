5edf2d033b8dab26917475a6b989135bd1763a90
elastic-pangolin <elastic-pangolin>
2026-01-11 18:15:05 +0100

    handle multiple functions in an option, using lambda functions again

--- game.py
+++ game.py
@@ -25,25 +25,48 @@ class Game:
         for screen in screenlist:
             lookup[screen["name"]] = index
             index += 1
-        print(lookup) # TODO DELTE ME
         screens = [None] * (len(screenlist)+1)
+        functions = {}
+        functionindex = 0
         for screen in screenlist:
             # [LB] are literal linebreaks
             layout = MenuLayout("", screen["header"] + "[LB][LB]" + screen["description"])
             for option in screen["options"]:
-                button_added = False
+                functions[functionindex] = [] # list of function to call for an option
                 for action in option["actions"]:
-                    if action["command"] == Command.TRANSITION:
+                    # parse messages
+                    if action["details"].get("message"):
+                        text = action["details"].get("message")
+                        functions[functionindex].append(lambda t=text: rpg.func_overlay("", t))
+                    # parse state-changing functions
+                    if action["command"] == Command.EXIT:
+                        ...
+                    elif action["command"] == Command.SAVE:
+                        ...
+                    elif action["command"] == Command.LOAD:
+                        ...
+                    elif action["command"] == Command.TRANSITION:
                         nextname = action["details"].get("nextScreen")
-                        layout.add_button(
-                            lambda: rpg.func_advance(lookup[nextname]),
-                            text=option["text"])
-                        button_added = True
-                        break
+                        functions[functionindex].append(lambda: rpg.func_advance(lookup[nextname]))
+                    elif action["command"] == Command.ADDITEM:
+                        itemname = action["details"].get("itemName")
+                        amount = action["details"].get("amount")
+                        failuremessage = action["details"].get("failureMessage")
+                        ...
+                    elif action["command"] == Command.LOOSEITEM:
+                        ...
+                    elif action["command"] == Command.NOOP:
+                        ...
                     else:
                         print(f"unknown action {str(action)}")
-                if not button_added:
-                    layout.add_button(lambda: None, text=option["text"])
+                    def _call_all(functions_list):
+                        counter = 1
+                        for f in functions_list:
+                            #print(f"FUNCTION_{counter} {f.__name__} {f.__code__.co_names} : {f.__code__.co_freevars} {[c.cell_contents for c in f.__closure__ or []]}")
+                            f()
+                            counter += 1
+                layout.add_button(lambda i=functionindex: _call_all(functions[i]), text=option["text"])
+                functionindex += 1
             screens[lookup[screen["name"]]] = layout
         
         return screens[1:] # index 0 is reserved for main menu
